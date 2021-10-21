6c4c79a60d70debcc92021eca63817bb79489892
ssilve1989 <steve@trumidtech.com>
2021-10-21 09:39:11 -0600

    wip: not working yet

--- trumid/ats/addition/main.ts
+++ trumid/ats/addition/main.ts
@@ -1,7 +1,12 @@
 import { NestFactory } from "@nestjs/core";
-import path from 'path';
 import { GrpcOptions, Transport } from "@nestjs/microservices";
+// TODO: YUUUUUCKKKKK need to figure out what to do with js_library generated packages and these kind of paths
+// the protogen in use will generate the nested directory under the package directory
+// maybe use protobufjs_library instead like cockroach and see what thats like
+import * as pb from '@nodez/addition-pb/pkg_pb/trumid/ats/addition/proto/addition_service_pb';
 import { AppModule } from "./app.module";
+
+console.log(pb);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +14,7 @@ async function bootstrap() {
   app.connectMicroservice<GrpcOptions>({
     options: {
       url: "localhost:5000",
-      protoPath: 'trumid/ats/addition/addition_service.proto',
+      protoPath: 'trumid/ats/addition/proto/addition_service.proto',
       package: 'trumid.ats.addition',
     },
     transport: Transport.GRPC,
